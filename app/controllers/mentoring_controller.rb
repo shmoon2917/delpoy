fc9393aa72851f3e0562890fe9da67f18cc7383e
유새람 <YouSairam@yusaelam-ui-MacBook-Pro.local>
2016-08-21 20:49:05 +0900

    mid mentor_view

--- app/controllers/mentoring_controller.rb
+++ app/controllers/mentoring_controller.rb
@@ -32,6 +32,8 @@ class MentoringController < ApplicationController
         i.user_id = params[:id_of_user]
         i.mentor_id = men.id
         i.list_id = @a.id  # 현재 user의 1:N관계에 있는 list의 몇번째 idx에 있을까요 
+        i.complete=0 #아무것도 아닌 상태이니까 0으로 우선 주어 줌 
+        
         cnt +=1
         i.save 
       end
@@ -51,6 +53,60 @@ class MentoringController < ApplicationController
   def mentor_view
     @user = current_user
     @find_my_apply= IndexOfApply.all
-    # @detail_content=
+    @all_user=User.all
+    
+  end
+  
+  def mentor_viewdetail
+    @user = current_user
+    
+    @details = ApplyListDetail.find(params[:list_id])
+    @mentee=User.find(@details.user_id)
+  end
+  
+  def reply_admit
+    @user = current_user
+    @list_id = params[:list_id]
+    # myindex = IndexOfApply.find_by(list_id: params[:list_id])
+    # myindex.complete=1
+    
+    # myindex.save
+  end
+  
+  def reply_admit2
+    @user=current_user
+    @list_id = params[:list_id]
+    
+    myindex = IndexOfApply.find_by(list_id: @list_id)
+    myindex.complete=1
+    myindex.auction_price=params[:cost]
+    myindex.save
+    
+    redirect_to '/mentor_view/apply_success?result=0'
+  end
+  
+  def reply_reject
+    @user=current_user
+    myindex=IndexOfApply.find_by(list_id: params[:list_id])
+    myindex.complete=3  #멘토링 거절시 compelte=>3으로
+    
+    myindex.save
+    
+    redirect_to '/mentor_view/apply_success?result=1'
+  end
+  
+  def reply_success
+    @user = current_user
+    if params[:result]=0
+      @print="수락되었습니다."
+    end
+    if params[:result]=1
+      @print="거절했습니다."
+    end
+    
+  end
+  def show
+    @user=current_user
+    @index=IndexOfApply.all
   end
 end
